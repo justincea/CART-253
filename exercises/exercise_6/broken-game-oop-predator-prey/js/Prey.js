7ff7ff07ae5d3f4b13e04cd927eb28b98ce3cc2d
Justin Luc Maxim Cea <justin.cea@yahoo.com>
2019-10-28 22:10:11 -0400

    Ex:6 Fixed
    Syntax Error with the value of the radius?

--- exercises/exercise_6/broken-game-oop-predator-prey/js/Prey.js
+++ exercises/exercise_6/broken-game-oop-predator-prey/js/Prey.js
@@ -82,7 +82,7 @@ class Prey { //FIXED
       noStroke();
       fill(this.fillColor);
       this.radius = this.health;
-      ellipse(this.x, this.y, this.radius * "two");
+      ellipse(this.x, this.y, this.radius * 2); //FIXED (Changed the "two" to "2")
       pop();
     }
 
